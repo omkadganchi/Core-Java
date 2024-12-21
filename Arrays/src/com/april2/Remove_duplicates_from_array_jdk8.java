f6257951a70ff4cb3b6d1ded61618964c5d6fd93
Om Kadganchi <114085272+omkadganchi@users.noreply.github.com>
2024-12-21 11:38:15 +0530

    Add files via upload

---
+++ Arrays/src/com/april2/Remove_duplicates_from_array_jdk8.java
@@ -0,0 +1,24 @@
+package com.april2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Remove_duplicates_from_array_jdk8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> numbers = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+		
+		List<Integer> uniqueNum = numbers.stream().distinct().collect(Collectors.toList());
+		
+		//stream() - convert list to stram
+		//distinct() - only unique elements can retained
+		//collect(Collectors.toList()) - collects the result back in the list 
+		
+			System.out.println(uniqueNum);
+		
+	}
+
+}
