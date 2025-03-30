eebba957932bc400da139056fb1de4087df337ed
Om Kadganchi <114085272+omkadganchi@users.noreply.github.com>
2025-03-30 15:19:04 +0530

    Add files via upload

---
+++ fibonacciSeries.java
@@ -0,0 +1,23 @@
+package com.tka;
+
+public class fibonacciSeries {
+
+	public static void main(String[] args) {
+
+		int num1 = 0;
+		int num2 = 1;
+		int num3;
+		
+		System.out.print(num1+" "+num2);
+		
+		for(int i=1; i<10; i++) {
+			num3 = num1 + num2;
+			System.out.print(" "+num3);
+			
+			num1 = num2;
+			num2 =  num3;
+		}
+		
+	}
+
+}
